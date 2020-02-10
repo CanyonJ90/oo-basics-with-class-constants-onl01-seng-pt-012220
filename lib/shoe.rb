86916dbe1ddaeb67552b9cf4caf8d450e670e9e9
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-10 03:03:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,20 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS= ["Uggs","Rainbow","Nike"]
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-def brand=(brand)
-  @brand = brand
-  BRANDS << brand
-  puts BRANDS(" ")
- end
- 
+
 end
